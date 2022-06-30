083f4c674e07f0b389332486bc97fe15a3abc37b
harumi <firmansyah720.fs@gmail.com>
2022-06-30 21:45:56 +0700

    add client url for cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000' // define client origin if both client and server have different origin
+        origin: process.env.CLIENT_URL || 'http://localhost:3000' // define client origin if both client and server have different origin
     }
 })
 // import socket function and call with parameter io
