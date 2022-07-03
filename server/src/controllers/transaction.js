fe04faeb4770815b615bd204eac4dbf6f53c16b4
harumi <firmansyah720.fs@gmail.com>
2022-07-03 18:16:15 +0700

    finnishing node mailer for transacntion

--- server/src/controllers/transaction.js
+++ server/src/controllers/transaction.js
@@ -1,5 +1,7 @@
 const {transaction, user, product, profile} = require('../../models');
 const midtransClient = require("midtrans-client");
+const nodemailer = require("nodemailer");
+const convertRupiah = require('rupiah-format');
 
 exports.addTransaction = async (req,res) => {
     try {
@@ -106,20 +108,17 @@ exports.notification = async (req,res) => {
 
                 if (transactionStatus == "capture") {
                     if (fraudStatus == "challenge") {
-                        // TODO set transaction status on your database to 'challenge'
-                        // and response with 200 OK
                         await updateTransaction("pending", orderId);
+                        await sendEmail("pending", orderId);
                         res.status(200);
                     } else if (fraudStatus == "accept") {
-                        // TODO set transaction status on your database to 'success'
-                        // and response with 200 OK
+                        await sendEmail("success", orderId);
                         await updateProduct(orderId);
                         await updateTransaction("success", orderId);
                         res.status(200);
                     }
                 } else if (transactionStatus == "settlement") {
-                    // TODO set transaction status on your database to 'success'
-                    // and response with 200 OK
+                    await sendEmail("success", orderId);
                     await updateTransaction("success", orderId);
                     res.status(200);
                 } else if (
@@ -127,45 +126,11 @@ exports.notification = async (req,res) => {
                     transactionStatus == "deny" ||
                     transactionStatus == "expire"
                 ) {
-                    // TODO set transaction status on your database to 'failure'
-                    // and response with 200 OK
+                    await sendEmail("failed", orderId);
                     await updateTransaction("failed", orderId);
                     res.status(200);
                 } else if (transactionStatus == "pending") {
-                    // TODO set transaction status on your database to 'pending' / waiting payment
-                    // and response with 200 OK
-                    await updateTransaction("pending", orderId);
-                    res.status(200);
-                }if (transactionStatus == "capture") {
-                    if (fraudStatus == "challenge") {
-                        // TODO set transaction status on your database to 'challenge'
-                        // and response with 200 OK
-                        await updateTransaction("pending", orderId);
-                        res.status(200);
-                    } else if (fraudStatus == "accept") {
-                        // TODO set transaction status on your database to 'success'
-                        // and response with 200 OK
-                        await updateProduct(orderId);
-                        await updateTransaction("success", orderId);
-                        res.status(200);
-                    }
-                } else if (transactionStatus == "settlement") {
-                    // TODO set transaction status on your database to 'success'
-                    // and response with 200 OK
-                    await updateTransaction("success", orderId);
-                    res.status(200);
-                } else if (
-                    transactionStatus == "cancel" ||
-                    transactionStatus == "deny" ||
-                    transactionStatus == "expire"
-                ) {
-                    // TODO set transaction status on your database to 'failure'
-                    // and response with 200 OK
-                    await updateTransaction("failed", orderId);
-                    res.status(200);
-                } else if (transactionStatus == "pending") {
-                    // TODO set transaction status on your database to 'pending' / waiting payment
-                    // and response with 200 OK
+                    await sendEmail("pending", orderId);
                     await updateTransaction("pending", orderId);
                     res.status(200);
                 }
@@ -356,3 +321,92 @@ exports.getTransaction = async (req,res) => {
         })
     }
 }
+
+
+const sendEmail = async (status, transactionId) => {
+    // Config service and email account
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.SYSTEM_EMAIL,
+            pass: process.env.SYSTEM_PASSWORD,
+        },
+    });
+
+    // Get transaction data
+    let data = await transaction.findOne({
+        where: {
+            id: transactionId,
+        },
+        attributes: {
+            exclude: ["createdAt", "updatedAt", "password"],
+        },
+        include: [
+            {
+                model: user,
+                as: "buyer",
+                attributes: {
+                    exclude: ["createdAt", "updatedAt", "password", "status"],
+                },
+            },
+            {
+                model: product,
+                as: "product",
+                attributes: {
+                    exclude: [
+                        "createdAt",
+                        "updatedAt",
+                        "idUser",
+                        "qty",
+                        "price",
+                        "desc",
+                    ],
+                },
+            },
+        ],
+    });
+
+    data = JSON.parse(JSON.stringify(data));
+
+    // Email options content
+    const mailOptions = {
+        from: process.env.SYSTEM_EMAIL,
+        to: data.buyer.email,
+        subject: "Payment status",
+        text: "Your payment is <br />" + status,
+        html: `<!DOCTYPE html>
+            <html lang="en">
+              <head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Document</title>
+                <style>
+                  h1 {
+                    color: brown;
+                  }
+                </style>
+              </head>
+              <body>
+                <h2>Product payment :</h2>
+                <ul style="list-style-type:none;">
+                  <li>Name : ${data.product.name}</li>
+                  <li>Total payment: ${convertRupiah.convert(data.price)}</li>
+                  <li>Status : <b>${status}</b></li>
+                </ul>  
+              </body>
+            </html>`,
+    };
+
+    // Send an email if there is a change in the transaction status
+    if (data.status != status) {
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) throw err;
+            console.log("Email sent: " + info.response);
+            return res.send({
+                status: "Success",
+                message: info.response,
+            });
+        });
+    }
+};
